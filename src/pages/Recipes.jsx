31e3e7cfeb436bd4018c67ffbf097cd936931303
Sagar Batham <shivsagar6912@gmail.com>
2026-04-10 12:03:11 +0530

    Adjust grid layout for recipe cards to improve responsiveness

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -17,7 +17,7 @@ function Recipes() {
       </div>
       
       {data.length > 0 ? (
-        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 auto-rows-max'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 auto-rows-max'>
           {renderrecipes}
         </div>
       ) : (
