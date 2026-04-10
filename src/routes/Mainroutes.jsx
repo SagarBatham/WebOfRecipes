d79931de018ede053a096a0d1337e9f7fbc1d2a9
Sagar Batham <shivsagar6912@gmail.com>
2026-04-10 12:10:09 +0530

    Refactor SingleRecipe component and update routing to new file structure

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -4,7 +4,7 @@ import Home from '../pages/Home'
 import Recipes from '../pages/Recipes'
 import About from '../pages/About'
 import CreateRecipes from '../pages/CreateRecipes'
-import SingleRecipe from '../pages/Singlerecipe'
+import SingleRecipe from '../pages/SingleRec'
 import PageNotFound from "../pages/PageNotFound";
 import Fav from "../pages/Fav";
 
